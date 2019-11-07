0a15202280e41aecc0d32e05ad436a58dd6086da
Alex Zimdahl <azimdahl91@gmail.com>
2019-11-07 14:31:32 -0500

    complete Hard Mode of activity

--- reactApp.js
+++ reactApp.js
@@ -1,52 +1,99 @@
 (function () {
 
-  class Team extends React.Component {
+  function Team(props) {
+    return (<div>
+      <h4>{props.teamData.name}!</h4>
+      <img alt="team logo" width={200} src={props.teamData.logo} />
+
+      <div>
+        {" "}Shots: {props.teamStats.shots + " "}
+        {" "}Score: {props.teamStats.score + " "}
+      </div>
+
+      <button onClick={props.handleShoot}>SHOOT</button>
+
+      {props.teamStats.shots > 0 &&
+        <div>
+          Shot Percentage: {(props.teamStats.score / props.teamStats.shots).toFixed(3)}
+        </div>
+      }
+    </div>)
+  }
+
+  class Game extends React.Component {
     constructor(props) {
       super(props)
 
       this.state = {
-        shots: 0,
-        score: 0,
+        homeTeamStats: {
+          shots: 0,
+          score: 0,
+        },
+
+        awayTeamStats: {
+          shots: 0,
+          score: 0
+        }
       }
 
       this.sound = new Audio("cheer.mp3")
     }
 
-    handleShoot = (event) => {
-      this.setState((state) => ({
-        shots: state.shots + 1
-      }))
+    handleShoot = (team, event) => {
+      const teamKey = `${team}Stats`;
+      let curTeam = this.state[teamKey];
+      curTeam.shots++;
       if (Math.random() >= .5) {
-        this.setState((state) => ({
-          score: state.score + 1,
-        }))
+        curTeam.score++;
         this.sound.play();
       }
+      this.setState({
+        [teamKey]: curTeam
+      })
+    }
+
+    handleReset = (event) => {
+      let homeTeam = this.state.homeTeamStats;
+      let awayTeam = this.state.awayTeamStats;
+      homeTeam.shots = 0;
+      homeTeam.score = 0;
+
+      awayTeam.shots = 0;
+      awayTeam.score = 0;
+      this.setState({
+        homeTeamStats: homeTeam,
+        awayTeamStats: awayTeam
+      })
     }
 
     render() {
-      return (<div backgroundimage={this.props.logo}>
-        <h4>Give it up for the {this.props.teamData.name}!</h4>
-        <img alt="team logo" width={200} src={this.props.teamData.logo} />
-        <div>{" "}Shots: {this.state.shots + " "}
-          {" "}Score: {this.state.score + " "}</div>
-        <button onClick={this.handleShoot}>SHOOT</button>
-        {this.state.shots > 0 &&
-          <div>Shot Percentage: {(this.state.score / this.state.shots).toFixed(3)}</div>
-        }
+      return (<div>
+        <h1>Welcome to {this.props.venue} Quidditch Match!</h1>
+        <div id="container">
+
+          <div id="homeTeam" className="teamStyle" style={{ backgroundColor: this.props.homeTeam.backgroundColor }}>
+            <Team
+              teamData={this.props.homeTeam}
+              handleShoot={() => this.handleShoot("homeTeam")}
+              teamStats={this.state.homeTeamStats}
+            />
+          </div>
+
+          <div id="awayTeam" className="teamStyle" style={{ backgroundColor: this.props.awayTeam.backgroundColor }}>
+            <Team
+              teamData={this.props.awayTeam}
+              handleShoot={() => this.handleShoot("awayTeam")}
+              teamStats={this.state.awayTeamStats}
+            />
+          </div>
+        </div>
+
+        <div>
+          <button onClick={this.handleReset}>RESET GAME</button>
+        </div>
+        
       </div>)
     }
-  }
-
-  function Game(props) {
-    return (<div>
-      <h1>Welcome to {props.venue} Quidditch Match!</h1>
-      This file represents the code after completing the setup step in the lab instructions
-      <div id="container">
-        <div id="homeTeam" className="teamStyle" style={{backgroundColor:props.homeTeam.backgroundColor}}><Team teamData={props.homeTeam}/></div>
-        <div id="awayTeam" className="teamStyle" style={{backgroundColor:props.awayTeam.backgroundColor}}><Team teamData={props.awayTeam}/></div>
-      </div>
-    </div>)
   }
 
   // Deafault App component that all other compents are rendered through
@@ -77,7 +124,7 @@
     }
 
     return (
-      <Game homeTeam={teamThreeObj} awayTeam={teamFourObj} venue="Hogwarts"/>
+      <Game homeTeam={teamThreeObj} awayTeam={teamFourObj} venue="Hogwarts" />
     )
   }
 
